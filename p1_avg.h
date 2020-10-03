fd9f18be62ab33117779fad5c6906a28e07f8d02
rjalvarez <69989382+rjalvarez80@users.noreply.github.com>
2020-10-03 12:24:06 -0600

    Update p1_avg.h

--- p1_avg.h
+++ p1_avg.h
@@ -6,8 +6,9 @@ void main_avg(void) {
   if(n == 3) {
     int x1, x2, x3;
     scanf("%d %d %d", &x1, &x2, &x3);
-    printf("%0.3lf\n", 1.0 * (x1 + x2 + x3) / 3);
-  } else {
+    printf("%0.3lf\n", 1.0 * (x1 + x2 + x3) / 3);           // "1.0 * " is add so the equation start as a floating instead of an integer.
+  } 
+  else {
     int x1, x2, x3, x4;
     scanf("%d %d %d %d", &x1, &x2, &x3, &x4);
     printf("%0.3lf\n", 1.0 * (x1 + x2 + x3 + x4) / 4);
