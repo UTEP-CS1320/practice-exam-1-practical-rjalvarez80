a64f5b540290f4a893bd852c4f8a39180437c459
rjalvarez <69989382+rjalvarez80@users.noreply.github.com>
2020-10-03 12:09:26 -0600

    Update p2_screen.h

--- p2_screen.h
+++ p2_screen.h
@@ -1,5 +1,37 @@
 #include <stdio.h>
 
 void main_screen(void) {
-  //TODO: Complete the code
+
+   long long phoneNumber;
+   int firstThree;
+   int middleThree;
+   int lastFour;
+   
+   scanf("%lld", &phoneNumber);
+  
+   firstThree = phoneNumber / 10000000;
+   middleThree = (phoneNumber % 10000000) / 10000;
+   lastFour = phoneNumber % 10000;
+  
+  if (firstThree != 915) {
+    if ( (firstThree == 617) && (middleThree == 253) ) {
+      printf("OK!\n");
+    }
+    else if ( ((firstThree == 718) && (middleThree == 834) && (lastFour == 9899)) || ((firstThree == 661) && (middleThree == 112) && (lastFour == 3581)) ) {
+      printf("OK!\n");
+    }
+    else {
+      printf("BLOCKED.\n");
+    }
+  }
+  if (firstThree == 915) {
+    if ( (middleThree == 701) && ((lastFour >= 1000) && (lastFour < 2000)) ) {
+      printf("BLOCKED.\n");
+    }
+    else {
+      printf("OK!\n");
+    }
+  }
+    
+   return 0;
 }
